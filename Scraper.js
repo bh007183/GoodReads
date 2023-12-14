a8b8b121e37ea8969008701c2dac9ea5a21cce4c
Ben Hopkins <bjhops17@gmail.com>
2023-12-14 14:30:27 -0800

    added class

---
+++ Scraper.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+class Scraper{
+
+    constructor(page){
+        this.page = page
+        this.loads = 0
+        this.allQuotes = []
+    }
+
+    //Takes a int to determin how many times it clicks on the load more button
+    //on mobile version.
+    async iPhoneLoadMore(numberOfLoads) {
+        if ((await this.page.locator(".jsLoadMore").isVisible()) && this.loads < numberOfLoads) {
+          this.loads++;
+          await this.page.locator(".jsLoadMore").click();
+          await this.page.waitForTimeout(10000);
+          console.log("Loading More....");
+          await this.iPhoneLoadMore();
+        } else {
+          await this.iPhoneScrapeData();
+          fs.appendFileSync("./test.json", JSON.stringify(this.allQuotes));
+        }
+      }
+
+      //Scrapes Data
+  async iPhoneScrapeData() {
+    console.log("Scraping Quotes...");
+    let quoteContainer = await this.page.locator(".quote").all();
+    console.log(quoteContainer)
+    await this.page.setDefaultTimeout(2000);
+    for (let v of quoteContainer) {
+      let quote;
+      let author;
+      let book;
+      let tags;
+      let likes;
+      try {
+        quote = await v.locator(".quoteBody").textContent();
+        author = await v.locator(".quoteAuthor").textContent();
+      } catch (err) {
+        console.log("Unable to find quote or author");
+      }
+      try {
+        book = await v.locator(".quoteBook").textContent();
+      } catch (err) {
+        console.log("Unable to find book");
+        book = null;
+      }
+      try {
+        tags = await v.locator(".quoteTags").textContent();
+      } catch (err) {
+        console.log("Unable to find tags");
+        tags = null;
+      }
+      try {
+        likes = await v.locator(".quoteLikes").textContent();
+      } catch (err) {
+        console.log("Unable to find number of likes");
+        likes = null;
+      }
+      let obj = {
+        quote,
+        author,
+        book,
+        tags,
+        likes,
+      };
+      this.allQuotes.push(obj);
+    }
+  }
+}
+
+module.exports = {Scraper}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const assert = require("node:assert");
 const { chromium, devices } = require("playwright");
 const fs = require("fs");
+ let {Scraper} = require("./Scraper");
 
 (async () => {
   // Setup
@@ -8,53 +9,15 @@ const fs = require("fs");
   const context = await browser.newContext(devices["iPhone 11"]);
   const page = await context.newPage();
   //
-  await page.goto(
-    "https://www.goodreads.com/author/quotes/4918776.Seneca"
-  );
+  await page.goto("https://www.goodreads.com/author/quotes/4918776.Seneca");
 
-  //   fs.appendFileSync("./test.json", JSON.stringify(quote))
-  let allQuotes = [];
-  let loads = 0;
+ let scraper = new Scraper(page)
 
-  // iPhoneLoadMore()
+  await scraper.iPhoneLoadMore(2)
 
-  // async function iPhoneLoadMore() {
-  //   if ((await page.locator(".jsLoadMore").isVisible()) && loads < 5) {
-  //     loads++;
-  //     await page.locator(".jsLoadMore").click();
-  //     await page.waitForTimeout(10000);
-  //     console.log("fired");
-  //     iPhoneLoadMore();
-  //   } else {
-  //     await iPhoneScrapeData();
-  //     fs.appendFileSync("./test.json", JSON.stringify(allQuotes));
-  //     await context.close();
-  //     await browser.close();
-  //   }
-  // }
+  //Scrapes Data
 
-  // async function iPhoneScrapeData() {
-    let quoteContainer = await page.locator(".quote").all();
-    for (v of quoteContainer) {
-      let quote = await v.locator(".quoteBody").textContent();
-      let author = await v.locator(".quoteAuthor").textContent();
-      let book = await v.locator(".quoteBook").textContent();
-      let tags = await v.locator(".quoteTags").textContent();
-      let likes = await v.locator(".quoteLikes").textContent();
-      let obj = {
-        quote,
-        author,
-        book,
-        tags,
-        likes
-      };
-      allQuotes.push(obj);
-      console.log(obj)
-    }
-  // }
-
-  //  <a class="next_page" rel="next" href="/author/quotes/17212.Marcus_Aurelius?page=2">next »</a>
-
-  //  let next = await page.locator("next_page").click()
+  await context.close();
+  await browser.close();
 })();
 
